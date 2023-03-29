7f0604219208f28bb3f8916d8ca10a942f9ab32c
Ahmed El-Brawany <ahmedelbrawany@gmail.com>
2023-03-29 14:13:24 +0200

    Fixing bugs In this commit, I fixed the following bugs:     - divide by zero bug.     - writing more than one floating point in a number bug.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const divide = ()=>op1/op2;
-const multiply = ()=>op1*op2;
-const subtract = ()=>op1-op2;
-const add = ()=>op1+op2;
+const divide = ()=>op1 / op2;
+const multiply = () => op1 * op2;
+const subtract = () => op1 - op2;
+const add = () => op1 + op2;
 
-function calculate(){
-    switch(operator){
+function calculate() {
+    switch (operator) {
         case '/': return divide();
         case 'x': return multiply();
         case '-': return subtract();
@@ -12,63 +12,91 @@ function calculate(){
     }
 }
 
+function containsOneOperator() {
+    let count = 0
+    for (let i = 0; i < resultScreen.textContent.length; i++) {
+        if ("+-/x=".includes(resultScreen.textContent[i])) {
+            count++;
+        }
+        if (count > 1) {
+            return false;
+        }
+    }
+    return count;
+}
 
+function clear(){
+    op1 = null;
+        op2 = null;
+        result = null;
+        operator = null;
+        opScreen.textContent = 0;
+        resultScreen.textContent = 0;
+}
+
+function showErrorMessage(){
+    alert("Can't divide by 0 !!!"); 
+    clear();
+}
 
 function display(event) {
 
     if ("+-/x=".includes(this.value)) {
-        resultScreen.textContent = `${op1} ${operator}`
-        if(!operator){
-            if(!op1){
-                op1 = parseFloat(opScreen.textContent);
+        if (containsOneOperator()) {
+            op2 = parseFloat(opScreen.textContent);
+            let prevOp1 = op1;
+            if(operator=="/" && op2==0){
+                showErrorMessage();
+                return;
             }
-            operator = this.value
-            resultScreen.textContent = `${op1} ${operator}`
-        }
-        else{
-            op2 = parseFloat(opScreen.textContent)
-            console.log(calculate());
-            result = Math.round(calculate()*100)/100;
-
-            if(this.value == "="){
-                resultScreen.textContent = `${op1} ${operator} ${op2} =`;
-                operator = null;
-            }
-            else{
+            op1 = calculate();
+            opScreen.textContent = op1;
+            if (this.value != "=") {
                 operator = this.value;
-                resultScreen.textContent = `${result} ${operator} `;
+                resultScreen.textContent = `${op1} ${operator}`;
+                flag = 1;
+            } else {
+                resultScreen.textContent = `${prevOp1} ${operator} ${op2} =`
             }
-            opScreen.textContent = result;
-            op1 = result;
-            op2 = null;
 
+        }
+        else if(this.value != "="){
             
+            op1 = parseFloat(opScreen.textContent);
+            operator = this.value;
+            
+                resultScreen.textContent = `${op1} ${operator} `
+            flag = 1;
         }
     }
-    else if(this.value=="clear"){
-        op1 = null;
-        op2 = null;
-        opScreen.textContent = 0;
-        resultScreen.textContent = "";
+    else if (this.value == "clear") {
+        
     }
-    else if(this.value=="delete"){
-        opScreen.textContent = opScreen.textContent.slice(0, opScreen.textContent.length-1)
-    }
-    else {
-
-        if (opScreen.textContent == "0" ||opScreen.textContent==`${op1}`) {
-            opScreen.textContent = this.value;
-            
+    else if (this.value == "delete") {
+        if (opScreen.textContent.length != 1) {
+            opScreen.textContent = opScreen.textContent.slice(0, opScreen.textContent.length - 1)
         }
         else {
-            opScreen.textContent += this.value;
+            opScreen.textContent = 0;
+        }
+    }
+    else {
+        if (!(this.value == "." && opScreen.textContent.includes(".") && !flag)) {
+            if (opScreen.textContent == "0" || flag) {
+                opScreen.textContent = this.value;
+                flag = 0;
+            }
+            else {
+                opScreen.textContent += this.value;
+
+            }
         }
     }
 }
 
 let op1 = null;
 let op2 = null;
-let result  = null;
+let flag = 0;
 let operator = null;
 
 let resultScreen = document.querySelector('.screen-result')
